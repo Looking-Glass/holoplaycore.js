66dbb54ae152c3efcebadce9b46e572e0eb67d36
bryanchrisbrown <bryanchristopherbrown@gmail.com>
2022-09-14 19:52:43 -0400

    update with webXR shader, update semver

--- dist/holoplaycore.module.js
+++ dist/holoplaycore.module.js
@@ -784,4 +784,70 @@ function generateRng() {
   return xoshiro128ss(seed(), seed(), seed(), seed());
 }
 
-export { CacheMessage, CheckMessage, Client, DeleteMessage, InfoMessage, InitMessage, Message, ShaderMessage, ShowCachedMessage, ShowMessage, UniformsMessage, WipeMessage };
+//turn the shader into valid glsl
+function glslifyNumbers(strings, ...values) {
+  let s = strings[0];
+  for (let i = 1; i < strings.length; ++i) {
+    const v = values[i - 1];
+    s += typeof v === 'number' ? v.toPrecision(10) : v;
+    s += strings[i];
+  }
+  return s;
+}
+
+// export the shader for use in WebXR // cfg is defined in @lookingglass/webxr
+function Shader(cfg) {
+  return glslifyNumbers`
+  // Shader copied from HoloPlay.js
+  precision mediump float;
+  uniform int u_viewType;
+  uniform sampler2D u_texture;
+  varying vec2 v_texcoord;
+  const float pitch    = ${cfg.pitch};
+  const float tilt     = ${cfg.tilt};
+  const float center   = ${cfg.calibration.center.value};
+  const float invView  = ${cfg.calibration.invView.value};
+  const float flipX    = ${cfg.calibration.flipImageX.value};
+  const float flipY    = ${cfg.calibration.flipImageY.value};
+  const float subp     = ${cfg.subp};
+  const float numViews = ${cfg.numViews};
+  const float tilesX   = ${cfg.quiltWidth};
+  const float tilesY   = ${cfg.quiltHeight};
+  const vec2 quiltViewPortion = vec2(
+    ${cfg.quiltWidth * cfg.tileWidth / cfg.framebufferWidth},
+    ${cfg.quiltHeight * cfg.tileHeight / cfg.framebufferHeight});
+  vec2 texArr(vec3 uvz) {
+    float z = floor(uvz.z * numViews);
+    float x = (mod(z, tilesX) + uvz.x) / tilesX;
+    float y = (floor(z / tilesX) + uvz.y) / tilesY;
+    return vec2(x, y) * quiltViewPortion;
+  }
+  float remap(float value, float from1, float to1, float from2, float to2) {
+    return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+  }
+  void main() {
+    if (u_viewType == 2) { // "quilt" view
+      gl_FragColor = texture2D(u_texture, v_texcoord);
+      return;
+    }
+    if (u_viewType == 1) { // middle view
+      gl_FragColor = texture2D(u_texture, texArr(vec3(v_texcoord.x, v_texcoord.y, 0.5)));
+      return;
+    }
+    vec4 rgb[3];
+    vec3 nuv = vec3(v_texcoord.xy, 0.0);
+    // Flip UVs if necessary
+    nuv.x = (1.0 - flipX) * nuv.x + flipX * (1.0 - nuv.x);
+    nuv.y = (1.0 - flipY) * nuv.y + flipY * (1.0 - nuv.y);
+    for (int i = 0; i < 3; i++) {
+      nuv.z = (v_texcoord.x + float(i) * subp + v_texcoord.y * tilt) * pitch - center;
+      nuv.z = mod(nuv.z + ceil(abs(nuv.z)), 1.0);
+      nuv.z = (1.0 - invView) * nuv.z + invView * (1.0 - nuv.z);
+      rgb[i] = texture2D(u_texture, texArr(vec3(v_texcoord.x, v_texcoord.y, nuv.z)));
+    }
+    gl_FragColor = vec4(rgb[0].r, rgb[1].g, rgb[2].b, 1);
+  }
+`;
+  }
+
+export { CacheMessage, CheckMessage, Client, DeleteMessage, InfoMessage, InitMessage, Message, Shader, ShaderMessage, ShowCachedMessage, ShowMessage, UniformsMessage, WipeMessage };
